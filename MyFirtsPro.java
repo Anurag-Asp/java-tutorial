884ed031d6707b4cacea73a880ceda50fe753fb6
Anurag-asp <mraspgaming@gmail.com>
2025-03-07 18:23:51 +0530

    first commit

---
+++ MyFirtsPro.java
@@ -0,0 +1,5 @@
+class MyFirtsPro{
+	public static void main(String args[]){
+		System.out.println("welcome to java");	
+	}
+}
---
+++ SumDemo.java
@@ -0,0 +1,7 @@
+class SumDemo{
+	public static void main(String args[]){
+		int a=25, b=30, add;
+		add=a+b;
+		System.out.println("The sum is "+add);
+	}
+}
